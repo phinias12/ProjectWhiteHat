48a1b9e5507163c001420b0abb50795ba84feaec
phinias12 <mendezphidias@gmail.com>
2014-12-30 21:29:54 -0500

    reading_example.py
    This file is to try and find out how I can read the file and replace a string.

---
+++ reading_example.py
@@ -0,0 +1,12 @@
+#! /usr/gin/python
+import string
+import os
+
+s = open ("C:\Users\Gateway\Desktop\hello.txt",
+          'r+')
+For ("hello") in s.readline(): 
+    print ("hello")
+    string.replace(str('hello','replacement variable'))
+    print ("hello")
+s.close()
+
